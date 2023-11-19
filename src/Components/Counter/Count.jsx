fcccf87fe65d5174c50ead6b5ee4c88b8dd7a5f7
Shipon Hossen Raju <msshipon234@gmail.com>
2023-11-19 12:36:58 +0600

    couter use Redux

--- src/Components/Counter/Count.jsx
+++ src/Components/Counter/Count.jsx
@@ -1,33 +1,39 @@
-import { useState } from "react";
+import { connect } from "react-redux";
+import { decrement, increment } from "../../Redux/Counter/Action";
 
-const Count = () => {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount((prev) => prev + 1);
-  };
-
-  const decrement = () => {
-    setCount((prev) => prev - 1);
-  };
-
+const Count = ({ count, increment, decrement }) => {
   return (
     <div className="flex items-center justify-between mx-5 max-w-xs my-5">
       <button
         onClick={increment}
         className="text-lg py-2 px-5 rounded-lg text-blue-950 bg-blue-400"
       >
-        Increment{" "}
+        Increment
       </button>
-      <strong className="text-2xl"> {count} </strong>
+
+      <strong className="text-2xl px-2"> {count} </strong>
+
       <button
         onClick={decrement}
         className="text-lg py-2 px-5 rounded-lg text-red-950 bg-red-400"
       >
-        Decrement{" "}
+        Decrement
       </button>
     </div>
   );
 };
 
-export default Count;
+const mapStateToProps = (state) => {
+  return {
+    count: state.value,
+  };
+};
+
+const mapDispatchToPros = (dispatch) => {
+  return {
+    increment: (value) => dispatch(increment(value)),
+    decrement: (value) => dispatch(decrement(value)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToPros)(Count);
--- src/Pages/Counter/Counter.jsx
+++ src/Pages/Counter/Counter.jsx
@@ -1,13 +1,14 @@
+import { connect } from "react-redux";
 import Count from "../../Components/Counter/Count";
+import { decrement, increment } from "../../Redux/Counter/Action";
 
 const Counter = () => {
   return (
     <section>
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-center items-center flex-col">
         <p> React Counter Systems </p>
         {/* counter component */}
-        <div className="py-6 my-4">
-          <Count />
+        <div className="py-2 my-1 flex flex-col gap-2">
           <Count />
         </div>
       </div>
